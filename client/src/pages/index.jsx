04f64f2aa6d0a1e47247dd3b934744b062665aeb
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-05-06 13:19:03 +0530

    first commit

---
+++ client/src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div className='text-7xl bg-green-600 p-5 rounded-2xl'>
+            WhatsApp Clone 
+        </div>
+    )
+}
+
+export default index
