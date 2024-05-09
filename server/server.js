3a07005b2072b0558458074283db01291600ab27
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-05-09 15:19:44 +0530

    add: auth and default route

--- server/server.js
+++ server/server.js
@@ -1,16 +1,40 @@
-import express from 'express';
-import dotenv from 'dotenv'
-import cors from 'cors'
+const express = require('express');
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+const AuthRoutes = require('./routes/authRoutes')
 
 dotenv.config()
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+
+// middleware 
+app.use(express.json()); // to parse json body
+app.use(
+    cors({
+        origin: "*",
+        credentials: true
+    })
+);
 
 
+
+// mount route
+app.use('/api/auth', AuthRoutes)
+
+
+// start server
 app.listen(PORT, () => {
     console.log(`Server Started on PORT ${PORT}`);
 });
+
+
+// Default Route
+app.get('/', (req, res) => {
+    console.log('Your server is up and running..!');
+    res.send(`<div>
+    This is Default Route  
+    <p>Everything is OK</p>
+    </div>`);
+})
