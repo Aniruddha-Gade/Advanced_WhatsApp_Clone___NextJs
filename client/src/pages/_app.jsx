a4670b60754449316b2f2c6b3569f1a5058ace0a
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-05-11 13:57:02 +0530

    add: context api

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+import { StateProvider } from "../context/stateContext";
+import Head from "next/head";
+import reducer, { initialState } from "@/context/stateReducers";
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return <StateProvider initialState={initialState} reducer={reducer} >
+        <Head>
+            <title>WhatsApp</title>
+            <link rel='shortcut icon' href='/favicon.png' />
+        </Head>
+        <Component {...pageProps} />;
+    </StateProvider>
+
+
 }
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,30 +1,39 @@
-import { CHECK_USER_ROUTE } from '@/utils/apiRoutes'
-import { firebaseAuth } from '@/utils/firebaseConfig'
+import React from 'react'
 import axios from 'axios'
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import React from 'react'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+
+import { firebaseAuth } from '@/utils/firebaseConfig'
 import { FcGoogle } from 'react-icons/fc'
+import { reducerCases } from '@/context/constants'
+import { useStateProvider } from '@/context/stateContext'
+import { CHECK_USER_ROUTE } from '@/utils/apiRoutes'
 
 const Login = () => {
 
     const router = useRouter()
+    const [{ }, dispatch] = useStateProvider()
 
     const handleLogin = async () => {
         const provider = new GoogleAuthProvider
-        const { user: { email, displayName: name, photoUrl: profileImage } } = await signInWithPopup(firebaseAuth, provider)
+        const { user: { email, displayName: name, photoURL: profileImage } } = await signInWithPopup(firebaseAuth, provider)
 
 
         try {
             const { data } = await axios.post(CHECK_USER_ROUTE, { email })
             console.log("CHECK USER API RESPONSE ==> ", data)
 
-            if(!data.status){
+            if (!data.status) {
+                dispatch({ type: reducerCases.SET_NEW_USER, newUser: true })
+                dispatch({
+                    type: reducerCases.SET_USER_INFO,
+                    userInfo: { name, email, profileImage, status: '' }
+                });
                 router.push('/onboarding')
             }
         } catch (error) {
-
+            console.log("Error while checking user register or not 🔴🔴 = ", error)
         }
 
     }
