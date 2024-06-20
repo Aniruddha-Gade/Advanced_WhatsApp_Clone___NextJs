f3770fa7a155a42dd31cd0170c276ff22fee10dc
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-06-20 21:23:15 +0530

    add: 'call, videoCall, search, menu' icons on Chat Header

--- client/src/components/chat/ChatHeader.jsx
+++ client/src/components/chat/ChatHeader.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import Avatar from '../common/Avatar'
+
+// icons
+import { MdCall } from 'react-icons/md'
+import { IoVideocam } from 'react-icons/io5'
+import { BiSearchAlt2 } from 'react-icons/bi'
+import { BsThreeDotsVertical } from 'react-icons/bs'
 
 const ChatHeader = () => {
   return (
-    <div>ChatHeader</div>
+    <div className='bg-panel-header-background h-16 px-4 py-3 z-10 flex justify-between items-center'>
+      <div className="flex-center gap-6">
+        {/* user profile image */}
+        <Avatar
+          type='sm'
+          image={'/profile'}
+        />
+        {/* user name and online/offline status */}
+        <div className="flex flex-col">
+          <span className="text-primary-strong">Demo username</span>
+          <span className="text-secondary text-sm">
+            online
+          </span>
+        </div>
+      </div>
+
+      {/* icons */}
+      <div className='flex gap-6'>
+        <MdCall className='text-panel-header-icon cursor-pointer text-xl' />
+        <IoVideocam className='text-panel-header-icon cursor-pointer text-xl' />
+        <BiSearchAlt2 className='text-panel-header-icon cursor-pointer text-xl' />
+        <BsThreeDotsVertical className='text-panel-header-icon cursor-pointer text-xl' />
+      </div>
+
+    </div>
   )
 }
 
