92bacd2341bd25c666d5cf7036f50bc97ae4f77c
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-06-21 09:49:22 +0530

    add: styles to chat container

--- client/src/components/chat/ChatContainer.jsx
+++ client/src/components/chat/ChatContainer.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const ChatContainer = () => {
   return (
-    <div>ChatContainer</div>
+    <div className='h-[80vh] w-full relative overflow-auto flex-grow custom-scrollbar '>
+      <div className="bg-chat-background w-full h-full fixed opacity-20 left-0 top-0 z-0">
+        <div className="flex-w-full">
+          <div className="flex flex-col justify-end w-full gap-1 overflow-auto">
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
