9c14ff111921058805952acf9ad4f57988ff0d32
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-06-22 12:48:45 +0530

    add: message send empty handler function

--- client/src/components/chat/MessageBar.jsx
+++ client/src/components/chat/MessageBar.jsx
@@ -1,32 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BsEmojiSmile } from 'react-icons/bs'
 import { ImAttachment } from 'react-icons/im'
 import { MdSend } from 'react-icons/md'
 import { FaMicrophone } from 'react-icons/fa'
+import { useStateProvider } from '@/context/stateContext'
 
 const MessageBar = () => {
+
+  const [{ userInfo, currentChatUser }, dispatch] = useStateProvider()
+  const [message, setMessage] = useState()
+  const sendMessage = () => {
+   
+  }
+
   return (
     <div className='h-20 flex items-center gap-6 relative bg-panel-header-background px-4 '>
       <>
         <div className="flex gap-6">
-          <BsEmojiSmile className='text-panel-header-icon cursor-pointer text-xl ' title='Emoji' />
+          <BsEmojiSmile
+            className='text-panel-header-icon text-xl cursor-pointer hover:text-panel-header-icon-hover active:scale-90'
+            title='Emoji'
+          />
           <ImAttachment
-            className='text-panel-header-icon cursor-pointer text-xl '
+            className='text-panel-header-icon text-xl cursor-pointer hover:text-panel-header-icon-hover active:scale-90'
             title='Attach File'
           />
         </div>
 
         <div className='h-10 w-full rounded-lg flex items-center'>
-          <input type='text' placeholder='Type a message' className='bg-input-background h-10 text-white focus:outline-none px-5 py-4 w-full' />
+          <input
+            type='text'
+            placeholder='Type a message'
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className='bg-input-background h-10 text-white focus:outline-none px-5 py-4 w-full'
+          />
         </div>
 
         <div className='flex-center w-10 '>
           <MdSend
-            className='text-panel-header-icon cursor-pointer text-xl '
+            onClick={sendMessage}
+            className='text-panel-header-icon text-xl cursor-pointer hover:text-panel-header-icon-hover active:scale-90'
             title='Send Message'
           />
           {/* <FaMicrophone
-            className='text-panel-header-icon cursor-pointer text-xl '
+            className='text-panel-header-icon text-xl cursor-pointer hover:text-panel-header-icon-hover active:scale-90'
             title='Record'
           /> */}
         </div>
